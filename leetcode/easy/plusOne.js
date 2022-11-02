e7417e10ccf025ab5612ab77befff08dd0e22f0d
kiprop-dave <davekip16@gmail.com>
2022-11-02 17:47:10 +0300

    plus one

---
+++ leetcode/easy/plusOne.js
@@ -0,0 +1,18 @@
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+const plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
+console.log(plusOne([9, 9, 9]));
