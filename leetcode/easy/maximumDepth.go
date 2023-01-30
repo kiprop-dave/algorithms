0f8d73510a805a82633eb66d0b37470df3abc191
kiprop-dave <davekip16@gmail.com>
2023-01-30 15:50:29 +0300

    maximum depth of binary tree go

---
+++ leetcode/easy/maximumDepth.go
@@ -0,0 +1,23 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    left := 1 + maxDepth(root.Left)
+    right := 1 + maxDepth(root.Right)
+    if left > right {
+        return left
+    }
+    return right
+}
