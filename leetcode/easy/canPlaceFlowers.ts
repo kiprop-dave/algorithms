77118aa9ea1092a7d58a431b74f21f8b29780160
kiprop-dave <davekip16@gmail.com>
2023-03-20 09:24:24 +0300

    can place flowers

---
+++ leetcode/easy/canPlaceFlowers.ts
@@ -0,0 +1,11 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  let empty = 0;
+  for(let i = 0; i < flowerbed.length; i++){
+    if(flowerbed[i] === 0 && (i === 0 || flowerbed[i-1] === 0) && (i === flowerbed.length - 1 || flowerbed[i+1] === 0)){
+      flowerbed[i] = 1;
+      empty++;
+    }
+    if(empty >= n) return true
+  }
+  return false;
+};
