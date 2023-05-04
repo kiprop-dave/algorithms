a81cf0614869f8967f40754ace2e3d526ede5150
kiprop-dave <davekip16@gmail.com>
2023-05-04 19:25:17 +0300

    restore the string using the indices array

---
+++ leetcode/easy/restoreString.ts
@@ -0,0 +1,16 @@
+/*
+You are given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+*/
+
+
+function restoreString(s: string, indices: number[]): string {
+  let output = new Array<string>(s.length);
+  for(let i = 0;i < indices.length;i++){
+    let index = indices[i];
+    output[index] = s[i];
+  }
+  return output.join("");
+};
